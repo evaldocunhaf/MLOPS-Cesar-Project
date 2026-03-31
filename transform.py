e1ede59c47e55761a7767651a644af018c2b2487
Evaldo Cunha F <evaldocunhaf@gmail.com>
2026-03-31 11:15:33 -0300

    Caminho de csv corrigido na classe de transformer

--- transform.py
+++ transform.py
@@ -3,6 +3,6 @@ from transformer import StepTransformer
 if __name__ == "__main__":
     transformer = StepTransformer(['age', 'gender', 'daily_gaming_hours', 'game_genre',
        'primary_game', 'gaming_platform', 'sleep_hours', 'sleep_quality',
-       'sleep_disruption_frequency', 'face_to_face_social_hours_weekly', 'grades_gpa'])
-    transformer.transform("./data/processed/Gaming and Mental Health.csv")
+       'sleep_disruption_frequency', 'face_to_face_social_hours_weekly', 'academic_work_performance'])
+    transformer.transform("./data/raw/Gaming and Mental Health.csv")
 
